c2be79581f0924f4233b80f29343cc1426d6e4b9
manesil <manesil.msm@gmail.com>
2022-10-31 19:12:43 -0300

    Versão 1 - projeto modulo 1

--- pgms/POO_Projeto_Modulo1_Banco/POO_Projeto_Modulo1_Banco/src/br/com/upsk/banco/cliente/Cliente.java
+++ pgms/POO_Projeto_Modulo1_Banco/POO_Projeto_Modulo1_Banco/src/br/com/upsk/banco/cliente/Cliente.java
@@ -1,4 +1,54 @@
 package br.com.upsk.banco.cliente;
 
+import br.com.upsk.banco.conta.Conta;
+
+import java.util.ArrayList;
+
 public class Cliente {
+
+    private String idCliente;
+    private String tipoCliente;
+    private String nomeCliente;
+    public ArrayList<String> idConta;
+
+
+    public Cliente(){
+
+    }
+
+    public Cliente(String idCliente, String tipo, String nomeCliente){
+        setIdCliente(idCliente);
+        setTipoCliente(tipo);
+        setNomeCliente(nomeCliente);
+    }
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getTipoCliente() {
+        return tipoCliente;
+    }
+
+    public void setTipoCliente(String tipoCliente) {
+        this.tipoCliente = tipoCliente;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public void abrirConta(String tipo){
+        Conta conta = new Conta();
+        //TODO Arraylist com abrirConta
+    }
+
 }
